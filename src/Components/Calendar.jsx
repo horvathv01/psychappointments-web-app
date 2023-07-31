959f662a617216e7ec91e5a5fbf93958892fc3ef
horvathv01 <horvathv01@gmail.com>
2023-07-31 16:58:26 +0200

    add external library for weekly calendar for main functionality

---
+++ src/Components/Calendar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { DayPilotCalendar } from "@daypilot/daypilot-lite-react";
+
+export default function Calendar(){
+    return(
+        <div>
+            <DayPilotCalendar/>
+        </div>
+    );
+}
